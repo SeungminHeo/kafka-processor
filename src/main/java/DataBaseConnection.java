1f4de4f0b343f93d74b6d2dc9bf8a53d03601870
허승민 <huhjay6@gmail.com>
2020-09-28 02:36:33 +0900

    Initialize Java Project

---
+++ src/main/java/DataBaseConnection.java
@@ -0,0 +1,10 @@
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.math.BigDecimal;
+import java.sql.*;
+import java.util.Date;
+
+class DataBaseConnection {
+
+}
---
+++ src/main/java/LogSimulator.java
@@ -0,0 +1,14 @@
+import java.math.BigDecimal;
+
+import org.json.JSONObject;
+import org.json.JSONException;
+
+import java.sql.*;
+import java.util.Date;
+
+public class LogSimulator {
+
+    LogSimulator() {
+    }
+
+}
