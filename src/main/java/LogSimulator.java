4c6d7e1c4b107f60d930946d017a1b29b8f7c2a0
SEUNGMIN <hawi75@yonsei.ac.kr>
2020-10-02 02:47:51 +0900

    Add configuration for Kafka

--- src/main/java/LogSimulator.java
+++ src/main/java/LogSimulator.java
@@ -1,14 +1,38 @@
-import java.math.BigDecimal;
-
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.json.JSONObject;
-import org.json.JSONException;
+import utils.KafkaJsonSerializer;
 
 import java.sql.*;
-import java.util.Date;
+import java.util.Properties;
 
 public class LogSimulator {
 
+    ResultSet logData;
+    Properties props;
+
+    LogSimulator(String brokerServers) {
+        try {
+            props = configureProducer(brokerServers);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     LogSimulator() {
+        try {
+            props = configureProducer("jolp-kafka-001:9092,jolp-kafka-002:9092,jolp-kafka-003:9092");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Properties configureProducer(String kafkaBrokerServers) {
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", kafkaBrokerServers);
+        properties.put("acks", "all");
+        properties.put("compression.type", "snappy");
+        return properties;
     }
 
 }
