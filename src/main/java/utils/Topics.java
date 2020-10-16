b970e5b410fba8777952e575b36d537f728843b2
SEUNGMIN <hawi75@yonsei.ac.kr>
2020-10-17 01:21:05 +0900

    Configure Kafka Topics

--- src/main/java/utils/Topics.java
+++ src/main/java/utils/Topics.java
@@ -11,8 +11,27 @@ public enum Topics {
     CLICK_LOG {
         @Override
         public String toString() { return "click_log"; }
+    },
+    ORDER_COMPLETE_LOG {
+        @Override
+        public String toString() { return "order_complete_log"; }
+    },
+    SEARCH_LOG {
+        @Override
+        public String toString() { return "search_log"; }
+    },
+    CART_LOG {
+        @Override
+        public String toString() { return "cart_log"; }
+    },
+    CLICK_RANKING {
+        @Override
+        public String toString() { return "click_ranking"; }
+    },
+    CLICK_MATRIX {
+        @Override
+        public String toString() { return "click_matrix"; }
     };
-
 
     public String topicName(){
         return this.toString();
